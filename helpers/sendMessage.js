2512d8ee24fa3e9eab81d8f1783dca7791547e0f
Edwin <edwin1999997@gmail.com>
2024-04-18 10:59:46 -0600

    All rest

--- helpers/sendMessage.js
+++ helpers/sendMessage.js
@@ -1,5 +1,5 @@
-const accountSid = 'AC89e14c3c22f894148399c5bcb85d67a0' // El id de tu cuenta; 
-const authToken = '1807e4ef99c87be8cfe72b7d738e930c' // El TOKEN de tu cuenta; 
+const accountSid = '' // El id de tu cuenta; 
+const authToken = '' // El TOKEN de tu cuenta; 
 const client = require('twilio')(accountSid, authToken);
 
 
