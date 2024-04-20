c11820016bdfbc6d152aaa83425a4b24690d4384
Edwin <edwin1999997@gmail.com>
2024-04-20 11:03:02 -0600

    HADSUH

--- controllers/pastillas.js
+++ controllers/pastillas.js
@@ -92,7 +92,7 @@ const EvaluarPastilla = async (req, res = response) => {
 
     // Si hay pastillas para tomar, enviar la cadena con los IDs
     if (pastillasParaTomar.length > 0) {
-        const names = pastillasParaTomar.map(pastilla => pastilla.name).join('');
+        const names = pastillasParaTomar.map(pastilla => pastilla.nombre).join('');
         res.status(200).json({ mensaje: `prend${names}` });
     } else {
         res.status(200).json({ mensaje: 'noprend' });
